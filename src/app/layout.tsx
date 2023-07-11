c7653f2c347dc3f9777c6253c9182c3c141eb95d
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-07-11 11:41:49 +0530

    Changed the layout file

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,22 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'JsonPress - JSON powered CMS',
-  description: 'A JSON powered CMS',
-}
+  title: "JsonPress - JSON powered CMS",
+  description: "A JSON powered CMS",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
     </html>
-  )
+  );
 }
