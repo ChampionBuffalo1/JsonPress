dbdfa0fe0f64e9ed15cdb65b3fc44a63ed191f83
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-07-13 22:18:00 +0530

    removed overlay

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className={inter.className + "  overflow-hidden"}>
         <SideBar />
         <GlobalProvider>
           {/* NavUser used within NavBar component requires redux-store*/}
