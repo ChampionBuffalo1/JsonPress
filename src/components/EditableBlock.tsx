b51b909ac0e3402a6760d4edf63bf9f5d769170f
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-07-17 22:57:44 +0530

    Using a different approach for rendering children

--- src/components/EditableBlock.tsx
+++ src/components/EditableBlock.tsx
@@ -1,26 +1,54 @@
 "use client";
 
-import { ComponentPropsWithRef, forwardRef } from "react";
-import BlockMapping, { AttributeType, MappingKey, MappingType } from "./Tags";
+import BlockMapping, { MappingKey } from "./Tags";
+import { ComponentPropsWithRef, ForwardedRef, forwardRef } from "react";
 
-interface EditableBlockProps extends ComponentPropsWithRef<"div"> {
+type EditableBlockProps = {
   type: MappingKey;
-  attribute: AttributeType<MappingKey>;
-}
+  attributes: Record<string, unknown>;
+} & ComponentPropsWithRef<"div">;
 
-export default forwardRef<HTMLDivElement, EditableBlockProps>(
-  function EditableBlock({ type, attribute, ...props }, ref) {
-    const Component = BlockMapping[
-      type
-    ] as MappingType[typeof type]["component"];
-    if (!Component) return null;
+function EditableBlock(
+  { type, attributes, ...props }: EditableBlockProps,
+  ref: ForwardedRef<HTMLDivElement>
+) {
+  const Component = BlockMapping[type]?.component || type;
+  if (!Component) return null;
+
+  if (attributes?.children) {
     return (
-      <div ref={ref} contentEditable={!attribute.disableEditable} {...props}>
-        {/* @ts-ignore - I cannot be bothered to fix the typing */}
-        <Component {...attribute}>
-          {attribute.children ? attribute.children : null}
-        </Component>
-      </div>
+      <>
+        <div {...props} ref={ref}>
+          {/* @ts-expect-error: Need to figure out a way to get rid of heading and paragraph without breaking types */}
+          <Component {...attributes}>
+            {/* @ts-expect-error */}
+            {attributes.children.map((block, key) => {
+              if (typeof block === "string") return block;
+
+              return (
+                <EditableBlock
+                  type={block.type}
+                  attributes={block.attributes}
+                  key={key}
+                  //
+                />
+              );
+            })}
+            {attributes.content && attributes.content}
+          </Component>
+        </div>
+      </>
     );
   }
-);
+  return (
+    <>
+      <div {...props} ref={ref}>
+        {/* @ts-expect-error: Need to figure out a way to get rid of heading and paragraph without breaking types */}
+        <Component {...attributes} />
+        {attributes.content && attributes.content}
+      </div>
+    </>
+  );
+}
+
+export default forwardRef<HTMLDivElement, EditableBlockProps>(EditableBlock);
