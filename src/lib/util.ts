abc82a0011c6f050edd643d55f61a4ac5fb3f198
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-07-20 14:54:49 +0530

    generating uuid

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,6 +1,10 @@
+import { v4 } from "uuid";
 import { clsx, ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+export function uuid() {
+  return v4().substring(0, 8);
 }
