84296c59e027fb43bf0384e28679b4a18329980f
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-07-20 14:11:18 +0530

    Fixed the List component

--- src/components/Tags/List.tsx
+++ src/components/Tags/List.tsx
@@ -1,54 +1,75 @@
 "use client";
 import { cn } from "@/lib/util";
-import React, { RefObject, createRef, useCallback, useState } from "react";
+import React, {
+  useRef,
+  useState,
+  RefObject,
+  createRef,
+  useCallback,
+} from "react";
 
 interface ListProps {
+  items: string[];
   type: "ol" | "ul";
-  children: string[];
   className?: string;
 }
 
-export default function List({
-  type,
-  className,
-  children,
-  ...props
-}: ListProps) {
-  const liRefs: RefObject<HTMLLIElement>[] = [];
-  const [currentIndex, setIndex] = useState(0);
+export default function List({ type, className, items, ...props }: ListProps) {
+  const liRefs = useRef<RefObject<HTMLLIElement>[]>([]);
+  const [children, setChildren] = useState<string[]>(items || []);
+
   const handleListChange = useCallback(
-    (event: React.KeyboardEvent<HTMLOListElement>) => {
+    (event: React.KeyboardEvent<HTMLLIElement>, currentIndex: number) => {
+      if (event.key === "Enter") {
+        event.preventDefault();
+        if (currentIndex === children.length - 1) {
+          setChildren((prevChild) => [...prevChild, ""]);
+          liRefs.current[currentIndex + 1]?.current?.focus();
+          return;
+        }
+        liRefs.current[currentIndex + 1]?.current?.focus();
+      }
+
       if (
-        (currentIndex !== liRefs.length - 1 && event.key === "ArrowDown") ||
-        (currentIndex !== 0 && event.key === "ArrowUp")
+        (event.key === "ArrowDown" && currentIndex !== children.length - 1) ||
+        (event.key === "ArrowUp" && currentIndex !== 0)
       ) {
         event.stopPropagation();
         const idx = currentIndex + (event.key === "ArrowDown" ? 1 : -1);
-        if (liRefs[idx]?.current?.focus) setIndex(idx);
-        liRefs[idx]?.current?.focus();
+        liRefs.current[idx]?.current?.focus();
+      }
+
+      if (
+        currentIndex !== 0 &&
+        event.key === "Backspace" &&
+        !event.currentTarget.textContent
+      ) {
+        setChildren((prevChilds) =>
+          prevChilds.filter((_, i) => i !== currentIndex)
+        );
+        liRefs.current[currentIndex - 1]?.current?.focus();
       }
     },
-    [currentIndex, liRefs]
+    [liRefs, children]
   );
 
   const ListComponent = type;
 
   return (
-    <ListComponent
-      contentEditable
-      onKeyDown={handleListChange}
-      className={cn(className, "w-full mx-8")}
-      {...props}
-    >
+    <ListComponent className={cn(className, "w-full mx-8")} {...props}>
       {children.map((item: string, key: number) => {
         const ref = createRef<HTMLLIElement>();
-        liRefs.push(ref);
+        liRefs.current[key] = ref;
         return (
           <li
-            onClick={() => setIndex(key)}
-            key={key}
-            className={type === "ul" ? "list-disc" : "list-decimal"}
             ref={ref}
+            key={key}
+            contentEditable
+            onKeyDown={(e) => handleListChange(e, key)}
+            className={cn(
+              "outline-none list-decimal",
+              type === "ul" && "list-disc"
+            )}
           >
             {item}
           </li>
