6263e61913844ec48e1b7cd0e8b0dccb6c4d9316
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-07-20 23:25:48 +0530

    Need to style the button properly

--- src/components/Editor.tsx
+++ src/components/Editor.tsx
@@ -1,28 +1,10 @@
 "use client";
 
-import { useCallback } from "react";
 import EditableBlock from "./EditableBlock";
 import { useAppSelector } from "@/app/hooks";
 
 export default function Editor() {
   const blocks = useAppSelector((state) => state.editor.blocks);
-
-  const handleKeyDown = useCallback(
-    (event: React.KeyboardEvent<HTMLDivElement>, index: number) => {
-      let idx: number | undefined;
-      if (event.key === "Enter" || event.key === "ArrowDown") {
-        event.preventDefault();
-        // idx = Math.min(index + 1, itemRefs.current.length - 1);
-      }
-      if (event.key === "ArrowUp") idx = Math.max(index - 1, 0);
-      if (idx !== undefined) {
-        // setTakeInput(false);
-        // setFilterOpts("");
-        // itemRefs.current[idx]?.focus();
-      }
-    },
-    []
-  );
 
   return (
     <div className="mt-10 flex flex-col items-center justify-center pt-10">
@@ -30,14 +12,22 @@ export default function Editor() {
         {blocks.map((block, key) => (
           <div className="w-full max-w-5xl flex mt-2 items-center" key={key}>
             <EditableBlock
+              id={block.id}
               // @ts-expect-error: I haven't fixed the typing yet
               type={block.type}
-              attributes={block.attributes}
-              onKeyDown={(e) => handleKeyDown(e, key)}
               currentIndex={key}
+              attributes={block.attributes}
             />
           </div>
         ))}
+        <button
+          className="w-14 rounded-md border border-gray-700 bg-slate-400 min-w-fit"
+          onClick={() => {
+            window.dispatchEvent(new Event("dispatch"));
+          }}
+        >
+          Save
+        </button>
       </div>
     </div>
   );
