1f27ab6a10a075b3bb56c314205f3d0ac1325401
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-07-21 15:38:21 +0530

    need a little bit of change

--- src/components/Tags/Table.tsx
+++ src/components/Tags/Table.tsx
@@ -1,25 +1,46 @@
 "use client";
 import { Plus } from "lucide-react";
-import { ComponentPropsWithoutRef, useMemo, useState } from "react";
+import { useAppDispatch } from "@/app/hooks";
+import { updateContent } from "@/app/reducer/editor";
+import { ComponentPropsWithoutRef, useEffect, useState } from "react";
 
 interface TableProps extends ComponentPropsWithoutRef<"table"> {
+  id: string;
   numRow?: number;
   numCol?: number;
   children: Array<string[]>;
 }
 
 export default function Table({
+  id,
   numRow,
   numCol,
   children,
   ...props
 }: TableProps) {
+  const dispatch = useAppDispatch();
   const [rowCount, setRowCount] = useState(numRow || children.length);
   const [colCount, setColCount] = useState(numCol || children[0].length);
-  const rows = useMemo(
-    () => Array.from({ length: rowCount }, () => Array(colCount).fill("")),
-    [rowCount, colCount]
+  const [rows, setRows] = useState(
+    children.length > 0
+      ? children
+      : Array.from({ length: rowCount }, () => Array(colCount).fill(""))
   );
+
+  useEffect(() => {
+    const handleDispatch = () => {
+      console.log("dispatching", rows);
+      dispatch(
+        updateContent({
+          id,
+          type: "table",
+          value: rows,
+        })
+      );
+    };
+    window.addEventListener("dispatch", handleDispatch);
+    return () => window.removeEventListener("dispatch", handleDispatch);
+  }, [id, rows, dispatch]);
 
   return (
     <div className="flex items-center m-2">
@@ -35,10 +56,28 @@ export default function Table({
                   key={cellIndex}
                   className="border border-gray-600 px-3 py-1 outline-none w-24"
                   contentEditable
+                  onInput={(e) => {
+                    const value = e.currentTarget.textContent || "";
+                    setRows((prevRows) =>
+                      prevRows.map((row, i) =>
+                        i === rowIndex
+                          ? row.map((cell, j) =>
+                              j === cellIndex ? value : cell
+                            )
+                          : row
+                      )
+                    );
+                  }}
                   onKeyDown={(e) => {
                     if (e.key === "Enter") {
                       e.preventDefault();
-                      if (rowIndex === rowCount - 1) setRowCount(rowCount + 1);
+                      if (rowIndex === rowCount - 1) {
+                        setRowCount(rowCount + 1);
+                        setRows((prevRows) => [
+                          ...prevRows,
+                          Array(colCount).fill(""),
+                        ]);
+                      }
                     }
                   }}
                 >
@@ -53,7 +92,10 @@ export default function Table({
       <Plus
         size={24}
         className="m-2 cursor-pointer border bg-gray-300 rounded-md"
-        onClick={() => setColCount(colCount + 1)}
+        onClick={() => {
+          setColCount(colCount + 1);
+          setRows((prevRows) => prevRows.map((row) => [...row, ""]));
+        }}
       />
     </div>
   );
