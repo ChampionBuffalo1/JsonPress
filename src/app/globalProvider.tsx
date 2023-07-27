4f92c6df3411384089f99bc41704e4bb26076c2f
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-07-27 22:26:15 +0530

    Added SWR Provider

--- src/app/globalProvider.tsx
+++ src/app/globalProvider.tsx
@@ -1,13 +1,23 @@
 // https://nextjs.org/docs/getting-started/react-essentials#context
 "use client";
 
-import { Provider } from "react-redux";
 import { store } from "./store";
+import { SWRConfig } from "swr";
+import { Provider } from "react-redux";
+
+const swrOptions = {
+  fetcher: (resource: string, init: RequestInit) =>
+    fetch(resource, init).then((res) => res.json()),
+};
 
 export default function GlobalProvider({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <SWRConfig value={swrOptions}>{children}</SWRConfig>
+    </Provider>
+  );
 }
