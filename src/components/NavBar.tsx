b6dc4aaaa65a9a9b3a8bbfc8a03aee87e86dcc90
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-07-27 22:27:08 +0530

    Updated the navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,11 @@
-import NavUser from "./NavUser";
+"use client";
+import { cn } from "@/lib/util";
 import { FileJson } from "lucide-react";
+import { useAppSelector } from "@/app/hooks";
 
 export default function NavBar() {
+  const user = useAppSelector((state) => state.user);
+  const isLoggedIn = user.token;
   return (
     <nav className="fixed top-0 z-50 w-full border-b bg-gray-800 border-gray-600">
       <div className="px-3 py-3 lg:px-5 lg:pl-3">
@@ -14,11 +18,33 @@ export default function NavBar() {
               </span>
             </a>
           </div>
-          <div className="flex items-center">
-            <NavUser />
+          <div className="w-1/12 justify-between flex">
+            {!isLoggedIn && <Button className="mx-10" name="Login" />}
+            {user.role !== "normal" && (
+              <Button name="Create User" href="/create" />
+            )}
           </div>
         </div>
       </div>
     </nav>
+  );
+}
+
+interface ButtonProps {
+  name: string;
+  href?: string;
+  className?: string;
+}
+function Button({ name, href, className }: ButtonProps) {
+  return (
+    <a
+      href={href || `/${name.toLowerCase()}`}
+      className={cn(
+        "px-3 py-2 text-sm font-medium text-white rounded-md hover:bg-gray-700 border border-blue-500",
+        className
+      )}
+    >
+      <button>{name}</button>
+    </a>
   );
 }
