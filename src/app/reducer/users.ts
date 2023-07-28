b320dcee7f8501a88aaceaceb9247a1fbe1bde42
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-07-28 20:59:15 +0530

    Fixed the user reducer

--- src/app/reducer/users.ts
+++ src/app/reducer/users.ts
@@ -1,23 +1,32 @@
 import { createSlice } from "@reduxjs/toolkit";
 import type { PayloadAction } from "@reduxjs/toolkit";
 
+type FlattenObject<T extends Record<string, unknown>> = {
+  [K in keyof T]: T[K] extends Record<string, unknown>
+    ? { [P in keyof T[K]]: T[K][P] }
+    : T[K];
+};
+
 type Role = "normal" | "admin" | "manager";
 
-export interface UserState {
-  id: string;
-  role: Role;
-  name: string;
+export interface UserState extends FlattenObject<Payload["user"]> {
   token: string;
-  socialMedia?: {
-    website?: string;
-    twitter?: string;
-    facebook?: string;
-    instagram?: string;
-  };
 }
+type Payload = {
+  token: string;
+  user: {
+    name: string;
+    role: Role;
+    socialMedia?: {
+      website?: string;
+      twitter?: string;
+      facebook?: string;
+      instagram?: string;
+    };
+  };
+};
 
 const initialState: UserState = {
-  id: "",
   name: "",
   role: "normal",
   token: "",
@@ -28,12 +37,11 @@ export const userSlice = createSlice({
   name: "user",
   initialState,
   reducers: {
-    setUser(state, action: PayloadAction<UserState>) {
-      state.id = action.payload.id;
-      state.name = action.payload.name;
-      state.role = action.payload.role;
+    setUser(state, action: PayloadAction<Payload>) {
       state.token = action.payload.token;
-      state.socialMedia = action.payload.socialMedia ?? {};
+      state.name = action.payload.user.name;
+      state.role = action.payload.user.role;
+      state.socialMedia = action.payload.user.socialMedia ?? {};
     },
   },
 });
