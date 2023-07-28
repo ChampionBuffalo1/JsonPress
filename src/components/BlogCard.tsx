3c8b511443d9b54ad3f9a8393111f5f25b19eb6f
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-07-28 23:27:44 +0530

    Using next/link so redux doesn't reset

--- src/components/BlogCard.tsx
+++ src/components/BlogCard.tsx
@@ -1,4 +1,5 @@
 import { MoveRight } from "lucide-react";
+import Link from "next/link";
 
 type BlogCardProps = {
   data: {
@@ -29,7 +30,7 @@ export default function BlogCard({ data }: BlogCardProps) {
   return (
     <div className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       {data.coverImage && (
-        <a href={href}>
+        <Link href={href} prefetch={false}>
           <img
             loading="lazy"
             decoding="async"
@@ -37,25 +38,26 @@ export default function BlogCard({ data }: BlogCardProps) {
             className="rounded-t-lg"
             alt="Cover image for the blog"
           />
-        </a>
+        </Link>
       )}
       <div className="p-5">
-        <a href={href}>
+        <Link href={href} prefetch={false}>
           <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
             {data.title}
           </h5>
-        </a>
+        </Link>
         <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 pb-2">
           {data.description || "No description"}
         </p>
         <div className="flex justify-between">
-          <a
+          <Link
             href={href}
+            prefetch={false}
             className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
           >
             Read more
             <MoveRight size={20} className="mx-1" />
-          </a>
+          </Link>
           <p className="text-gray-700 left-24 dark:text-gray-300">
             {data.views} views
           </p>
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Link from "next/link";
 import { cn } from "@/lib/util";
 import { FileJson } from "lucide-react";
 import { useAppSelector } from "@/app/hooks";
@@ -11,12 +12,12 @@ export default function NavBar() {
       <div className="px-3 py-3 lg:px-5 lg:pl-3">
         <div className="flex items-center justify-between">
           <div className="flex items-center justify-start">
-            <a href="/" className="flex ml-2 md:mr-24">
+            <Link href="/" className="flex ml-2 md:mr-24">
               <FileJson className="h-9 mr-3" color="#ffffff" size={28} />
               <span className="self-center text-xl font-semibold sm:text-2xl whitespace-nowrap dark:text-white">
                 JsonPress
               </span>
-            </a>
+            </Link>
           </div>
           <div className="w-1/12 justify-between flex">
             {!isLoggedIn && <Button className="mx-10" name="Login" />}
@@ -37,7 +38,7 @@ interface ButtonProps {
 }
 function Button({ name, href, className }: ButtonProps) {
   return (
-    <a
+    <Link
       href={href || `/${name.toLowerCase()}`}
       className={cn(
         "px-3 py-2 text-sm font-medium text-white rounded-md hover:bg-gray-700 border border-blue-500",
@@ -45,6 +46,6 @@ function Button({ name, href, className }: ButtonProps) {
       )}
     >
       <button>{name}</button>
-    </a>
+    </Link>
   );
 }
