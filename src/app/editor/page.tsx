6e775a3c5b69bae366f9a7023ca824a4f602d03f
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-07-31 12:56:29 +0530

    Only rendering the create panel if user is loggedIn

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -1,5 +1,20 @@
+"use client";
+
 import Editor from "@/components/Editor";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function EditorPage() {
-  return <Editor />;
+  const [render, setRender] = useState<boolean>(false);
+  const navigator = useRouter();
+  useEffect(() => {
+    const user = localStorage.getItem("user");
+    if (!user) {
+      navigator.push("/login");
+    } else {
+      setRender(true);
+    }
+  }, [setRender, navigator]);
+
+  return render ? <Editor /> : <></>;
 }
