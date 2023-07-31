f025fcf0340d386b1bce3d82091f0632c2b1014e
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-07-31 14:31:16 +0530

    Added no blogs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
     <div className="container mx-auto mt-10">
       <div className="flex flex-row-reverse ml-4 p-2">
         <Link href="/editor" prefetch={false}>
-          <button className="border bg-green-800 rounded-md min-w-fit min-h-fit w-20 h-10 text-gray-300 text-lg">
+          <button className="border bg-green-600 rounded-md min-w-fit min-h-fit w-20 h-10 text-gray-100 text-lg border-slate-800">
             Create
           </button>
         </Link>
@@ -32,11 +32,16 @@ export default function Home() {
           <h1>Error: {error}</h1>
         </div>
       )}
-      {data && (
+      {data?.blogs.length > 0 && (
         <div className="grid grid-cols-4 gap-4 lg:grid-cols-4 sm:grid-cols-2">
           {data.blogs.map((blog: any) => (
             <BlogCard key={blog._id} data={blog} />
           ))}
+        </div>
+      )}
+      {data?.blogs.length === 0 && (
+        <div className="flex justify-center items-center pt-2 text-2xl text-slate-900">
+          <h1>No blogs found</h1>
         </div>
       )}
     </div>
