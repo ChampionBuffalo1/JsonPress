a6b8b4493fad738b4ef2008499c69fda02a7fffa
ChampionBuffalo1 <vidish7nov@gmail.com>
2023-08-01 13:01:24 +0530

    Added axios Instance

--- src/lib/util.ts
+++ src/lib/util.ts
@@ -1,4 +1,6 @@
+import axios from "axios";
 import { v4 } from "uuid";
+import { apiHost } from "./Constants";
 import { clsx, ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
 
@@ -8,3 +10,20 @@ export function cn(...inputs: ClassValue[]) {
 export function uuid() {
   return v4().substring(0, 8);
 }
+
+export const apiInstance = axios.create({
+  baseURL: apiHost,
+  maxRedirects: 1,
+  headers: {
+    "Content-Type": "application/json",
+  },
+  transformRequest: [
+    (data, headers) => {
+      const token = JSON.parse(localStorage.getItem("user") || "{}").token;
+      if (token) {
+        headers.set("Authorization", `Bearer ${token}`);
+      }
+      return data;
+    },
+  ],
+});
